e99529ca5c6082f76be1c0dd54e46219644208b1
anilamustafaa <anila.mustafa3@student.uni-pr.edu>
2026-04-16 19:22:36 +0200

    Implementimi i funksionit per konvertimin e karaktereve Base32 ne bits

--- Dekoderi.py
+++ Dekoderi.py
@@ -1,2 +1,9 @@
 alfabeti_dhe_numrat = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+def base32_to_bits(text: str) -> str:
+  bits = ""
+  for char in text:
+    index = alfabeti_dhe_numrat.index(char)
+    bits += f"{index:05b}"
+    return bits
 def decode_base32(text: str) -> bytes:
+  
