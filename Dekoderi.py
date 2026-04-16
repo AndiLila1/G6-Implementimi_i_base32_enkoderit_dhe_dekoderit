9870bee8f693211495cee524d90af2a57eacf44e
Sara Vishi <vishisara@gmail.com>
2026-04-16 21:01:48 +0200

    Implementimi i funksionit për konvertimin e bits në bytes

--- Dekoderi.py
+++ Dekoderi.py
@@ -5,5 +5,14 @@ def base32_to_bits(text: str) -> str:
     index = alfabeti_dhe_numrat.index(char)
     bits += f"{index:05b}"
     return bits
+
+def bits_to_bytes(bits: str) -> bytes:
+    rezultat = bytearray()
+    for i in range(0, len(bits), 8):
+        byte_chunk = bits[i:i+8]
+        if len(byte_chunk) == 8:
+            rezultat.append(int(byte_chunk, 2))
+    return bytes(rezultat)
+    
 def decode_base32(text: str) -> bytes:
   
