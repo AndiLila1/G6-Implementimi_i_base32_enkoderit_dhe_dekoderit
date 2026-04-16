1d2ca3f2b68fb04fab837b9c2e88eaeffb260454
anilamustafaa <anila.mustafa3@student.uni-pr.edu>
2026-04-17 01:11:36 +0200

    perfundimi i logjikes se dekoderit Base32

--- Dekoderi.py
+++ Dekoderi.py
@@ -4,7 +4,7 @@ def base32_to_bits(text: str) -> str:
   for char in text:
     index = alfabeti_dhe_numrat.index(char)
     bits += f"{index:05b}"
-    return bits
+ return bits
 
 def bits_to_bytes(bits: str) -> bytes:
     rezultat = bytearray()
@@ -15,4 +15,7 @@ def bits_to_bytes(bits: str) -> bytes:
     return bytes(rezultat)
     
 def decode_base32(text: str) -> bytes:
+  text = text.strip().replace(" ","").upper()
+  bits = base32_to_bits(text)
+return bits_to_bytes(bits)
   
